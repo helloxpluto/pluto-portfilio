2720b2b3eba9bc97d6277dd623ff39458b896381
helloxpluto <deplanetart@gmail.com>
2020-12-20 18:02:39 -0800

    restarting machine after vscode glitch

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -8,9 +8,9 @@ const nav = () => {
 			<Navbar bg='light' variant='light'>
 				<Navbar.Brand href='/'>Angel Rene Meillon </Navbar.Brand>
 				<Nav className='mr-auto'>
-					<Nav.Link href='#home'>Home</Nav.Link>
-					<Nav.Link href='#features'>Features</Nav.Link>
-					<Nav.Link href='#pricing'>Pricing</Nav.Link>
+					<Link to='/' class='nav-link' > home </Link>
+					<Link to='/' class='nav-link' > resume </Link>
+					<Link to='/' class='nav-link' > contact me </Link>
 				</Nav>
 			</Navbar>
 		</div>
---
+++ src/components/Resume/resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { } from 'react-bootstrap';
+
+const resume = () => {
+    return (
+			<div>
+                
+
+
+				<a href='https://docs.google.com/document/d/1VMb4Gk4jIwBB9za0sLI1NAx27VqSf2LIRyj9jWbB3OE/edit?usp=sharing'> Resume Link</a>
+			</div>
+		);
+};
+
+export default resume;
